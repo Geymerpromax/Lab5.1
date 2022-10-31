9e345a2d16106cdd672c5f066df5612a83867d57
Руслан <@DESKTOP-V4KRFG3>
2022-10-31 23:13:04 +0300

    Добавьте файлы проекта.

---
+++ Lab5.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <random>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+   
+}
